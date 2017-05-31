16ce97eed4a06966022ac28c8730b0494b80b5d4
PCMasterRays <richard.dececio@gmail.com>
2017-05-31 13:14:59 +0100

    Site Files 1.0
    The Basic Site Files I have been working on, this isn't a start point, I
    have just decided to start using Git for version control.

---
+++ php/about.php
@@ -0,0 +1,19 @@
+<?php
+$page_title = "About";
+include('includes/header.html');
+?>
+
+<h1>About Us</h1>
+
+  <p>Self-Proclaimed computer enthusiast and aspiring software developer Richard Dececio created this site
+  to show his talent in Website construction and to serve as a mini project in HTML, PHP, CSS and MySQL,
+  If you would like to contact him see the details on the contact us page or send me a message on
+  GitHub at the link below!</p>
+
+<img class="img-circle" src="https://scontent.fbhx2-1.fna.fbcdn.net/v/t1.0-9/15193691_1788379461377031_7501563450416783168_n.jpg?oh=07bc49c6163317aff5a257133d250455&oe=59BCFF49" alt="An Image of Richard Dececio" height="140" width="140">
+
+<?php
+include('includes/footer.html');
+?>
+
+<!-- Richard Dececio 2017 -->
---
+++ php/gallery.php
@@ -0,0 +1,14 @@
+<?php
+$page_title = "Gallery";
+include('includes/header.html');
+?>
+
+<h1>Gallery Page</h1>
+<p>Page currently under construction</p>
+<p><strong>Coming Soon!</strong> A page of my photography, and PhotoShop skills!</p>
+
+<?php
+include('includes/footer.html');
+?>
+
+<!-- Richard Dececio 2017 -->
